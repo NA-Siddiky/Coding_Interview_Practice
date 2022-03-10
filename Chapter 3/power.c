3d756b1010e21d40930a5ef9f59596bda8156e3c
NA-Siddiky <siddiky.academic@gmail.com>
2022-03-11 00:18:49 +0600

    GCD 1

--- Chapter 3/power.c
+++ Chapter 3/power.c
@@ -3,7 +3,9 @@
 
 int main(void)
 {
-  for(int i = 1; i < 5; i++)
-     printf("pow(3.2, %d) = %lf\n", i, pow(3.2, i));  
-  return 0;
+    int x = 2;
+
+    printf("%d\n", (int)pow(x, 4));
+
+    return 0;
 }
